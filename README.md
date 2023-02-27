17c8777eae342047df00d270c587d1be8fa4d148
Josh Johanning <joshjohanning@github.com>
2023-02-27 14:43:43 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # actions-logs-warning-checker
+
+- Runs through recent[^1] workflow runs to see if there are any warnings in the logs
+- Outputs results to a specified CSV file
+- In the output CSV, there is a column to denote whether the warning was found in the latest workflow run or not
+
+## Usage
+
+1. Run `gh auth login` to authenticate with GitHub CLI
+2. Run `./generate-repos.sh <org> > repos.csv` 
+    - Modify list as needed
+    - Or create a list of repos in a csv file, `<org/<repo>`, 1 per line, with a trailing empty line at the end of the file
+3. Run: `./github-actions-warning-checker.sh repos.csv output.csv`
+
+## Example Output
+
+```csv
+repo,workflow_name,workflow_url,found_in_latest_workflow_run
+joshjohanning-org/actions-linter-testing,CI,https://github.com/joshjohanning-org/actions-linter-testing/blob/main/.github/workflows/blank.yml,no
+joshjohanning-org/actions-linter-testing,new-workflow,https://github.com/joshjohanning-org/actions-linter-testing/blob/main/.github/workflows/new-file.yml,yes
+```
+
+[^1]: Recent is defined as the last 5 workflow runs, modify `WORKFLOW_RUNS_TO_CHECK` as needed.
---
+++ github-actions-warning-checker.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# DOT NOT REMOVE TRAILING NEW LINE IN THE INPUT CSV FILE
+
+# Usage: 
+# Step 0: Run `gh auth login` to authenticate with GitHub CLI
+# Step 1: Run ./generate-repos.sh <org> > repos.csv
+#    (or create a list of repos in a csv file, 1 per line, with a trailing empty line at the end of the file)
+# Step 2: ./github-actions-warning-checker.sh repos.csv output.csv
+
+WORKFLOW_RUNS_TO_CHECK=5
+
+if [ $# -lt "2" ]; then
+    echo "Usage: $0 <repo_filename> <output_filename>"
+    exit 1
+fi
+
+if [ ! -f "$1" ]; then
+    echo "Repo input file $1 does not exist"
+    exit 1
+fi
+
+# mv output.log if it exists
+if [ -f "$2" ]; then
+    date=$(date +"%Y-%m-%d %T")
+    mv $2 "$2-$date.csv"
+fi
+
+echo "repo,workflow_name,workflow_url,found_in_latest_workflow_run" > output.csv
+
+filename="$1"
+
+while read -r repofull ; 
+do
+    IFS='/' read -ra data <<< "$repofull"
+
+    org=${data[0]}
+    repo=${data[1]}
+
+    echo $"> Checking repo for warnings in actions runs: $org/$repo"
+
+    # get each workflow id
+    workflows=$(gh api /repos/$org/$repo/actions/workflows)
+    echo $workflows | jq -c -r '.workflows[]' | while read -r workflow ; do
+        workflow_id=$(echo $workflow | jq -r '.id')
+        workflow_path=$(echo $workflow | jq -r '.path')
+        workflow_name=$(echo $workflow | jq -r '.name')
+        workflow_url=$(echo $workflow | jq -r '.html_url')
+        echo "  >> Checking workflow: $workflow_name ($workflow_path) $workflow_id"
+        # will get the most recent x workflow file run(s)
+        runs=$(gh api --method GET /repos/$org/$repo/actions/workflows/$workflow_id/runs -F per_page=$WORKFLOW_RUNS_TO_CHECK)
+        i=0
+        echo $runs | jq -c -r '.workflow_runs[]' | while read -r run ; do
+            run_id=$(echo $run | jq -r '.id')
+            run_display_title=$(echo $run | jq -r '.display_title')
+            echo "    >> Checking run: $run_display_title ($run_id)"
+            run_output=$(gh run view $run_id -R $org/$repo)
+            if [[ $run_output == *"deprecated and will be disabled soon"* ]]; then
+                echo "      >> !!! found deprecated message"
+                if [[ $i == 0 ]]; then
+                    latest="yes"
+                else
+                    latest="no"
+                fi
+                echo "$org/$repo,$workflow_name,$workflow_url,$latest" >> output.csv
+                break
+            fi
+            i=$((i+1))
+        done
+    done
+
+
+done < "$filename"
+
+
+
+# example of warning
+# Warning: The `set-output` command is deprecated and will be disabled soon. Please upgrade to using Environment Files. For more information see: https://github.blog/changelog/2022-10-11-github-actions-deprecating-save-state-and-set-output-commands/
